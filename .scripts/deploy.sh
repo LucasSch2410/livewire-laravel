ee3db85d1ece692a728367dd07796b7f8c396bd8
Lucas Schroeder <lucasschroeder2410@gmail.com>
2024-11-03 14:55:55 -0300

    feat(api): atualizacoes nas rotas

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,10 +4,16 @@ set -e
 
 echo "Deployment started ..."
 
+docker exec app php artisan down
+
 git pull origin production
 
-composer install --ignore-platform-reqs
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader --ignore-platform-reqs
 
-docker exec app php artisan migrate
+docker exec app php artisan migrate --force
+
+docker exec app php artisan optimize:clear
+
+docker exec app php artisan up
 
 echo "Deployment finished!"
