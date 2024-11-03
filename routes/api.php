22d7abc0e598ea6afbb84529b69d0801978a11a2
Lucas Schroeder <lucasschroeder2410@gmail.com>
2024-11-02 22:42:00 -0300

    feat(api): atualizacoes nas rotas

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,6 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::prefix('v1')->group(function () {
-    //Route::get('/users/{id}', [UserController::class, 'show']);
-//    Route::get('/users', [UserController::class, 'index']);
-//    Route::post('/users', [UserController::class, 'store']);
-});
-
 Route::post('login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
